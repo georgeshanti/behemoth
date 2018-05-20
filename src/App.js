20542a528ffb6672698ca923afc9ae41f761f2a0
Navin Mohan <navinmohan81@gmail.com>
2018-05-20 17:01:52 +0530

    Added sample routes with lazyloading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom'
 import './App.css';
+
+import asyncComponent from './utils/asyncComponent'
+// import Home from './pages/home'
+// import Events from './pages/events'
+import NavBar from './components/navbar'
+
+const HomeImport = () => import('./pages/home')
+const EventsImport = () => import('./pages/events')
+const TalksImport = () => import('./pages/talks')
+
+const Home = asyncComponent(HomeImport)
+const Events = asyncComponent(EventsImport)
+const Talks = asyncComponent(TalksImport)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar/>
+
+        <div className="content">
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/events" component={Events}/>
+          <Route exact path="/talks" component={Talks}/>
+        </div>
+        
       </div>
     );
   }
