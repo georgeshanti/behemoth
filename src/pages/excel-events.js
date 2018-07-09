81886b9fb64e2dc1ced768ef75ac38689a797ff5
nehamj <nehamj798@gmail.com>
2018-07-10 00:11:03 +0530

    Search box :v:

--- src/pages/excel-events.js
+++ src/pages/excel-events.js
@@ -16,6 +16,7 @@ export default class ExcelEvents extends Component {
 		this.fadeOutContents = this.fadeOutContents.bind(this)
 		this.fadeInContents = this.fadeInContents.bind(this)
 		this.hideContent = this.hideContent.bind(this)
+		this.filterList=this.filterList.bind(this)
 
 
 		//initialise timeline
@@ -26,7 +27,22 @@ export default class ExcelEvents extends Component {
 			activeEvent: null,
 			redirect : false,
 			redirectAbout: false,
+			initialItem:["event 1","event 2","event 3","event 4","event 5","event 6"],
+			items:[]
 		};
+	}
+
+    componentWillMount(){
+		this.setState({items:this.state.initialItem})
+	}
+
+	//searching
+	filterList(e){
+		var updatedList=this.state.initialItem;
+		updatedList=updatedList.filter(function( item){
+			return item.toLowerCase().search(e.target.value.toLowerCase())!==-1
+		});
+		this.setState({items:updatedList});
 	}
 
 	closeEvent(e) {
@@ -88,44 +104,63 @@ export default class ExcelEvents extends Component {
 	render() {
 		return(
 			<div className='container'>
+			    <div> <input type="text" placeholder="search"  onChange={this.filterList}/> </div>
 				<div id='eventsContainer' className='events-grid'>
-					<div id='event1' className=' event1 events' onClick={() =>this.showEvent('event1')}>
+				   {this.state.items.indexOf("event 1")!==-1?
+					  (<div id='event1' className=' event1 events' onClick={() =>this.showEvent('event1')}>
 					       <div className=' event1 big-numbers'>
 						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 1
-						   </div>
-					</div>
-					<div id='event2' className=' event2 events' onClick={() =>this.showEvent('event2')}>
+						   </div>	  
+					  </div>)
+					:("")
+				   }
+				   {this.state.items.indexOf("event 2")!==-1?
+					  (<div id='event2' className=' event2 events' onClick={() =>this.showEvent('event2')}>
 					       <div className=' event2 big-numbers'>
 						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 2
 						   </div>
-					</div>
-					<div id='event3' className=' event3 events' onClick={() =>this.showEvent('event3')}>
+					  </div>)
+					:("")
+				    }
+					{this.state.items.indexOf("event 3")!==-1?
+					  (<div id='event3' className=' event3 events' onClick={() =>this.showEvent('event3')}>
 					       <div className=' event3 big-numbers'>
 						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 3
 						   </div>
-					</div>
-					<div id='event4' className=' event4 events' onClick={() =>this.showEvent('event4')}>
+					   </div>)
+					:("")
+					}
+					{this.state.items.indexOf("event 4")!==-1?
+					  (<div id='event4' className=' event4 events' onClick={() =>this.showEvent('event4')}>
 					       <div className=' event4 big-numbers'>
 						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 4
 						   </div>
-					</div>
-					<div id='event5' className='event5 events ' onClick={() =>this.showEvent('event5')}>
+					  </div>)
+					:("") 
+				    }
+					{this.state.items.indexOf("event 5")!==-1? 
+					  (<div id='event5' className='event5 events ' onClick={() =>this.showEvent('event5')}>
 					       <div className='event5 big-numbers '>
 						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   
 						   Event 5
 						   </div>
-					</div>
-					<div id='event6' className='event6 events ' onClick={() =>this.showEvent('event6')}>
+					  </div>)
+					:("")
+				    }
+					{this.state.items.indexOf("event 6")!==-1?  
+					  (<div id='event6' className='event6 events ' onClick={() =>this.showEvent('event6')}>
 				           <div className='event6 big-numbers '>
 						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>						   
 						   Event 6
 						   </div>
-					</div>
+					  </div>)
+				  :("")
+				}
 				</div>
 				<div id='contentsContainer' className='events--expanded hidden'>
 					<div id='contents-close' className='btn-close' onClick={this.closeEvent}><img src={require('../img/close.png')}/></div>
