9fb70b197fb5d243af6b69211acf303e987d8743
Pallavi V G <pallavivg98@gmail.com>
2018-07-11 16:29:03 +0530

    Fix navbar

--- src/pages/excel-events.js
+++ src/pages/excel-events.js
@@ -91,50 +91,50 @@ export default class ExcelEvents extends Component {
 				<div id='eventsContainer' className='events-grid'>
 					<div id='event1' className=' event1 events' onClick={() =>this.showEvent('event1')}>
 					       <div className=' event1 big-numbers'>
-						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
+						   <img alt='' src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 1
 						   </div>
 					</div>
 					<div id='event2' className=' event2 events' onClick={() =>this.showEvent('event2')}>
 					       <div className=' event2 big-numbers'>
-						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
+						   <img  alt='' src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 2
 						   </div>
 					</div>
 					<div id='event3' className=' event3 events' onClick={() =>this.showEvent('event3')}>
 					       <div className=' event3 big-numbers'>
-						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
+						   <img alt='' src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 3
 						   </div>
 					</div>
 					<div id='event4' className=' event4 events' onClick={() =>this.showEvent('event4')}>
 					       <div className=' event4 big-numbers'>
-						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
+						   <img alt='' src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   Event 4
 						   </div>
 					</div>
 					<div id='event5' className='event5 events ' onClick={() =>this.showEvent('event5')}>
 					       <div className='event5 big-numbers '>
-						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
+						   <img alt='' src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>
 						   
 						   Event 5
 						   </div>
 					</div>
 					<div id='event6' className='event6 events ' onClick={() =>this.showEvent('event6')}>
 				           <div className='event6 big-numbers '>
-						   <img src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>						   
+						   <img alt='' src={require('../img/dummy-img.png')} className="grid-logo-image"/><br/>						   
 						   Event 6
 						   </div>
 					</div>
 				</div>
 				<div id='contentsContainer' className='events--expanded hidden'>
-					<div id='contents-close' className='btn-close' onClick={this.closeEvent}><img src={require('../img/close.png')}/></div>
-					<div id='event1-content' className='event-bg hidden'><CardsNew redirect={this.state.redirect} redirectAbout={this.state.redirectAbout}/></div>
-					<div id='event2-content' className='event-bg hidden'><CardsNew redirect={this.state.redirect} redirectAbout={this.state.redirectAbout}/></div>
-					<div id='event3-content' className='event-bg hidden'><CardsNew redirect={this.state.redirect} redirectAbout={this.state.redirectAbout}/></div>
-					<div id='event4-content' className='event-bg hidden'><CardsNew redirect={this.state.redirect} redirectAbout={this.state.redirectAbout}/></div>
-					<div id='event5-content' className='event-bg hidden'><CardsNew redirect={this.state.redirect} redirectAbout={this.state.redirectAbout}/></div>
-					<div id='event6-content' className='event-bg hidden'><CardsNew redirect={this.state.redirect} redirectAbout={this.state.redirectAbout}/></div>
+					<div id='contents-close' className='btn-close' onClick={this.closeEvent}><img  alt='' src={require('../img/close.png')}/></div>
+					<div id='event1-content' className='event-bg hidden'><CardsNew/></div>
+					<div id='event2-content' className='event-bg hidden'><CardsNew/></div>
+					<div id='event3-content' className='event-bg hidden'><CardsNew/></div>
+					<div id='event4-content' className='event-bg hidden'><CardsNew/></div>
+					<div id='event5-content' className='event-bg hidden'><CardsNew/></div>
+					<div id='event6-content' className='event-bg hidden'><CardsNew/></div>
 				</div>
 			</div>
 		);
