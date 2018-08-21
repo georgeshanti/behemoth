4214e13a4853dea60c70e2eed27132c015db4ab5
georgeshanti <georgeshanti@gmail.com>
2018-08-21 19:30:24 +0530

    Opening animation started

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 import NavButton from 'components/nav-button'
 import asyncComponent from './utils/asyncComponent'
 
+const HomeImport = () => import('pages/home')
 const EventsImport = () => import('pages/events')
 const ErrorImport = () => import('pages/error')
 
+const Home = asyncComponent(HomeImport)
 const Events = asyncComponent(EventsImport)
 const ErrorPage = asyncComponent(ErrorImport)
 
@@ -15,9 +17,16 @@ class App extends Component {
   render() {
     return (
       <div className="pageContainer">
+        <div className='background' >
+            <svg viewBox="-1000 -1000 2000 1000">
+                <circle cx="0" cy="200" r="1000" fill="#011935" />
+                <circle cx="0" cy="200" r="800" fill="#022041" />
+                <circle cx="0" cy="200" r="600" fill="#02244b" />
+            </svg>
+        </div>
         <div className="content">
           <Switch>
-            <Route path="/" exact render={()=> (<div className="hi"></div>)} />
+            <Route path="/" exact component={Home} />
             <Route path='/events' component={Events} />
             <Route path="/talks" render = {()=> (<h1>Talks</h1>)} />
             <Route path="/workshops" render = {()=> (<h1>Workshops</h1>)} />
