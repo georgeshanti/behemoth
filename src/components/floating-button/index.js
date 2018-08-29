af41a8a247750c4b443f9a045476bcd30af63a26
georgeshanti <georgeshanti@gmail.com>
2018-08-29 14:36:25 +0530

    Menu changed.

--- src/components/floating-button/index.js
+++ src/components/floating-button/index.js
@@ -4,11 +4,14 @@ import styles from './style.module.scss';
 
 class FloatingButton extends Component {
     render() {
+      console.log(this.props)
       return (
         <NavLink exact to={this.props.to} activeClassName={styles.active}>
           <div style={this.props.style} className={styles.FloatingButton} style={this.props.style} onClick={this.props.onClick}>
-            <i className={this.props.icon + " " + styles.icon} style={{verticalAlign: 'middle'}}></i>
-            <span className={styles['menu-text']}>{this.props.menuText}</span>
+            <span className={styles['menu-text']} style={{background: this.props.color}}>{this.props.text}</span>
+            <div className={styles["icon-holder"]}>
+              <i className={this.props.icon + " " + styles.icon} style={{verticalAlign: 'middle'}}></i>
+            </div>
           </div>
         </NavLink>
       );
