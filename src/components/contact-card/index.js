cd287adfd79cab8676f1098ab538aea6a65a8a93
Pallavi V G <pallavivg98@gmail.com>
2018-10-07 17:54:55 +0530

    Initial layout

--- src/components/contact-card/index.js
+++ src/components/contact-card/index.js
@@ -8,7 +8,10 @@ export default class ContactCard extends Component{
         return(
             <div>
             <div className={styles["card"]}>
+                <img src="https://placeimg.com/640/480/people"/>
+                <div className={styles["bottom-left"]}>
                     {this.props.details.name}
+                </div>
             </div>
             </div>
         )
