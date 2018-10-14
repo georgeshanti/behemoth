63dd9a8bc550f8e624f5838f68afc0aa8fcaa551
georgeshanti <georgeshanti@gmail.com>
2018-10-14 12:42:05 +0530

    Contact page issues fixed.

--- src/components/contact-card/index.js
+++ src/components/contact-card/index.js
@@ -16,12 +16,12 @@ export default class ContactCard extends Component{
                     {this.props.details.name}  <br />
                     <div className={styles["contact-info"]}>
                     <img  alt='email' src={require('../../img/message-closed-envelope.png')}/> <a href={"mailto:" + this.props.details.email}>{this.props.details.email}</a> <br />
-                    <img  alt='phone' src={require('../../img/phone-receiver.png')}/> {this.props.details.phone}  <br />
+                    <img  alt='phone' src={require('../../img/phone-receiver.png')}/> <a href={"tel:"+ this.props.details.phone}>{this.props.details.phone}</a><br />
                     </div>
                 </div>
-                <div className={styles["bottom-right"]}>
-                    <a href={this.props.details.github} target="_black"><img  alt='github' src={require('../../img/github-logo.png')}/></a>
-                    &nbsp;
+                <div className={styles["top-right"]}>
+                    {/* <a href={this.props.details.github} target="_black"><img  alt='github' src={require('../../img/github-logo.png')}/></a>
+                    &nbsp; */}
                     <a href={this.props.details.linkedin} target="_black"><img  alt='linkedin' src={require('../../img/linkedin-logo.png')}/></a>
                 </div>
                 <div className={styles["gradient-overlay"]}></div>
